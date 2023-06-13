8f77821466322a9a67d850c33c987a8ece8c657c
shripal <shripalp@gmail.com>
2023-06-13 15:14:47 -0600

    delete member fixed

--- components/Member.js
+++ components/Member.js
@@ -10,7 +10,7 @@ function Member(props) {
       }
     );
   };
-  handleDelete();
+  //handleDelete();
 
   return (
     <tr>
--- components/MembersFeed.js
+++ components/MembersFeed.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import Member from "./member";
+import Member from "./Member";
 
 const MembersFeed = () => {
   const [members, setMembers] = useState({});
